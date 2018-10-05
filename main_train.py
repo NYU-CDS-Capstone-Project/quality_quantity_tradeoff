0bd0e9fa4988e16aab6e7526597d3d5478e82064
Victor de Murat <vdm245@nyu.edu>
2018-10-05 14:34:34 -0400

    re-organizing to run experiments

--- main_train.py
+++ main_train.py
@@ -1,8 +1,19 @@
 from train import train_valid_model, Net
 from plot import plot_results
 import torch.optim as optim
+import argparse
 
-max_layer = 2
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', default='./../data', help='directory for data')
+parser.add_argument('--max_layer', default=2, help='possible number of layers in the CNN')
+parser.add_argument('--batch_size', default=4, help='batch size')
+
+opt = parser.parse_args()
+print(opt)
+
+
+max_layer = opt.max_layer
 layers = [[i, j] for i in range(max_layer) for j in range(max_layer)]
 print(layers)
 
@@ -14,7 +25,7 @@ for layer in layers:
     nb_params = sum(p.numel() for p in net.parameters())
 
     optimizer = optim.Adam(net.parameters(), lr=0.001)
-    acc = train_valid_model(net, 10, optimizer, True)
+    acc = train_valid_model(opt.data_dir, opt.batch_size, net, 10, optimizer, True)
     results.append({'layers':layer, 'num_params': nb_params, 'accuracy': acc})
 
 
--- train.py
+++ train.py
@@ -1,37 +1,13 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import numpy as np
 from torch.utils.data.sampler import SubsetRandomSampler
 
-import numpy as np
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-cat_idx = np.where(np.array(trainset.train_labels)==3)[0]
-dog_idx = np.where(np.array(trainset.train_labels)==5)[0]
-
 from torch.utils.data.dataset import Dataset
+import torch.nn as nn
+import torch.nn.functional as F
 
-cat_array = trainset.train_data[cat_idx]
-dog_array = trainset.train_data[dog_idx]
-train_data = np.concatenate([cat_array, dog_array])
-train_label = np.array([0]*cat_array.shape[0] + [1]*dog_array.shape[0])
 
 class Cifar2(Dataset):
     def __init__(self, data, label, transform):
@@ -44,35 +20,6 @@ class Cifar2(Dataset):
     def __len__(self):
         return self.label.shape[0]
 
-
-train_set = Cifar2(train_data, train_label, transform)
-
-
-batch_size = 4
-valid_size = 0.2
-random_seed = 10
-
-num_train = len(train_set)
-indices = list(range(num_train))
-split = int(np.floor(valid_size * num_train))
-
-np.random.seed(random_seed)
-np.random.shuffle(indices)
-
-train_idx, valid_idx = indices[split:], indices[:split]
-train_sampler = SubsetRandomSampler(train_idx)
-valid_sampler = SubsetRandomSampler(valid_idx)
-
-train_loader = torch.utils.data.DataLoader(
-    train_set, batch_size=batch_size, sampler=train_sampler,
-    num_workers=2)
-valid_loader = torch.utils.data.DataLoader(
-    train_set, batch_size=batch_size, sampler=valid_sampler,
-    num_workers=2)
-
-
-import torch.nn as nn
-import torch.nn.functional as F
 
 
 class Net(nn.Module):
@@ -99,9 +46,6 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
-
-criterion = nn.BCEWithLogitsLoss()
 
 
 def train(model, train_loader, optimizer):
@@ -131,7 +75,60 @@ def valid(model, valid_loader):
     return valid_loss/len(valid_loader), correct/len(valid_loader)
 
 
-def train_valid_model(model, num_epoch, optimizer, verbose = False):
+def train_valid_model(data_dir, batch_size, model, num_epoch, optimizer, verbose = False):
+    
+    transform = transforms.Compose(
+                [transforms.ToTensor(),
+                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root=data_dir, train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                             shuffle=False, num_workers=2)
+    
+    
+    classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    cat_idx = np.where(np.array(trainset.train_labels)==3)[0]
+    dog_idx = np.where(np.array(trainset.train_labels)==5)[0]
+    
+    cat_array = trainset.train_data[cat_idx]
+    dog_array = trainset.train_data[dog_idx]
+    train_data = np.concatenate([cat_array, dog_array])
+    train_label = np.array([0]*cat_array.shape[0] + [1]*dog_array.shape[0])
+    
+    
+    train_set = Cifar2(train_data, train_label, transform)
+
+
+    valid_size = 0.2
+    random_seed = 10
+
+    num_train = len(train_set)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    np.random.seed(random_seed)
+    np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, sampler=train_sampler,
+        num_workers=2)
+    valid_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, sampler=valid_sampler,
+        num_workers=2)
+    
+    criterion = nn.BCEWithLogitsLoss()
+    
+    
     for epoch in range(num_epoch):  # loop over the dataset multiple times
         best_acc = 0
         train_loss = train(model, train_loader, optimizer)
