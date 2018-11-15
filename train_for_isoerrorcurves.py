56686838c335c3c40959fa3027dc6116be419aed
Ruben <rns365@nyu.edu>
2018-11-15 16:45:42 -0500

    add test on best valid, append names to best models

--- train_for_isoerrorcurves.py
+++ train_for_isoerrorcurves.py
@@ -1,8 +1,10 @@
-from train import train_valid_model, Net
+from train import train_valid_model, Net, valid
 from plot import plot_results
 import torch.optim as optim
 import argparse
 import pickle
+import datetime
+datestr = datetime.datetime.now().strftime("%m_%d-%H_%M")
 
 from data import generate_cifar_loaders
 
@@ -10,7 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='./../data', help='directory for data')
 parser.add_argument('--max_layer', type=int, default=2, help='possible number of layers in the CNN')
 parser.add_argument('--batch_size', type=int, default=4, help='batch size')
-parser.add_argument('--epochs', type=int, default=10, help='num epoch')
+parser.add_argument('--epochs', type=int, default=50, help='num epoch')
 
 opt = parser.parse_args()
 
@@ -18,16 +20,24 @@ opt = parser.parse_args()
 max_layer = opt.max_layer
 results = []
 
-for quantity in [8000, 10000]:
-    for quality in [0, 0.05]:
-        net = Net(layers=[1, 1])
+quantities = []
+qualities = []
+num_iters = 1
+
+for quantity in quantities:
+    for quality in qualities:
+        net = Net(layers=[1, 2])
         optimizer = optim.Adam(net.parameters(), lr=0.001)
-        train_loader, valid_loader = generate_cifar_loaders(quantity, quality)
-        acc = train_valid_model(net, opt.epochs, optimizer, train_loader, valid_loader, verbose=True)
-        results.append({'quality':quality, 'quantity': quantity, 'accuracy': acc})
+        train_loader, valid_loader, test_loader = generate_cifar_loaders(quantity, quality)
+        acc = train_valid_model(net, opt.epochs, optimizer, train_loader,
+        valid_loader, datestr + 'best_model.pt', num_iters, verbose=False)
+
+        net.load_state_dict(torch.load(datestr + 'best_model.pt'))
+        test_acc = valid(net, test_loader,test=True)
+        results.append({'quality':quality, 'quantity': quantity, 'accuracy': test_acc})
 
 
-with open("isoerror_results.np", "wb") as fp:
+with open(datestr + "isoerror_results.np", "wb") as fp:
     pickle.dump(results, fp)
 
-plot_results("isoerror_results.np", 'test.png')
+#plot_results("isoerror_results.np", 'test.png')
